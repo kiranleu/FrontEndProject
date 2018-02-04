c2d0338d9b8657f0400c42dc8542117fa7f59265
Kira Anleu <kiraanleu@Kiras-iMac.local>
2018-02-04 10:49:04 +0000

    Added function hello, box alert !

---
+++ assets/js/custom.js
@@ -0,0 +1,5 @@
+function hello(){ // function keyword and the name of the function
+    alert("     This is the first step to meet your new You!    "); // code block
+}
+
+hello(); // calling the function. It triggers the function
